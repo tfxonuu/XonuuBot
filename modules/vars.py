8563f386b3f66094ef7958ca460cc7a034221b50
tfxonuu <iamtfsonu@gmail.com>
2025-10-18 12:56:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,20 +1,21 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#shahnawaz
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27293287"))
+API_HASH = environ.get("API_HASH", ""052041b03a7028b3bd11da3143cffa0e)
+BOT_TOKEN = environ.get("BOT_TOKEN", "8033732232:AAF_W5lgzZhFjTEZJ0qnMTxtnFqqUZTBuEo")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "7533484450"))
+CREDIT = environ.get("CREDIT", "𝕾𝖍𝖆𝖍𝖓𝖆𝖜𝖆𝖟")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7533484450').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7533484450').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
